0ef52c127796aaf7c423a78bca264d38ee9adbba
Nazmul-Babu <106833453+Nazmul-Babu@users.noreply.github.com>
2023-01-30 20:12:26 +0600

    marketing update

--- resources/views/user/pages/avatars/marketing.blade.php
+++ resources/views/user/pages/avatars/marketing.blade.php
@@ -106,23 +106,19 @@
 
                                                         @if (isset($generate))
                                                             <div class="p-5 mt-5 rounded bg-light-info text-dark fw-semibold mw-lg-700px text-start" data-kt-element="">
-
+                                                                      <p>{{ $title}}</p>
+                                                                    <p>{{ $generate}}</p>
 
                                                                 <form class="form-group" action="{{ route('SaveHistory')}}" method="POST">
                                                                     {{ csrf_field() }}
-                                                                    <input class="form-control" style="width: 100%" type="text" name="title" value="{{ $title}}"><br>
-                                                                    <input class="form-control" type="text" name="content" value="{{ $generate}}"><br>
-                                                                    {{-- <p>{{ $title}}</p> --}}
-                                                                    {{-- <p>{{ $generate}}</p> --}}
+                                                                    <input class="form-control" style="width: 100%" type="hidden" name="title" value="{{ $title}}"><br>
+                                                                    <input class="form-control" type="hidden" name="content" value="{{ $generate}}"><br>
+
 
                                                                     <input class="btn btn-success" type="submit" value="save">
 
                                                                 </form>
-                                                            {{-- <p id="">
 
-                                                                {{$generate}}
-
-                                                            </p> --}}
 
 
                                                             </div>
